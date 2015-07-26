d0ce8aa03ba771dc5dec029786de943907606e9e
Akira Chiku <akira.chiku@gmail.com>
2015-07-26 20:20:17 +0900

    remove win

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: MacOS',
         'Operating System :: Unix',
-        'Operating System :: Windows',
+        # 'Operating System :: Windows',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
