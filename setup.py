bc0ab53d66e35cfb1452c0a5a61ff9aeae756e32
Akira Chiku <akira.chiku@gmail.com>
2015-08-30 22:01:25 +0900

    Version dump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='jungle',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/achiku/jungle',
     license='MIT',
     author='Akira Chiku',
