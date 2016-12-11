6be3a40010b7256cb5b8fadfe4ef40b6c5691a06
Akira Chiku <akira.chiku@gmail.com>
2016-12-11 19:02:13 +0900

    Add error message when wrong AWS Profile Name is given

--- jungle/session.py
+++ jungle/session.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
+import sys
+
 import boto3
+import botocore
+import click
 
 
 def create_session(profile_name):
-    if not profile_name:
+    if profile_name is None:
         return boto3
     else:
-        return boto3.Session(profile_name=profile_name)
+        try:
+            session = boto3.Session(profile_name=profile_name)
+            return session
+        except botocore.exceptions.ProfileNotFound as e:
+            click.echo("Invalid profile name: {0}".format(profile_name, e), err=True)
+            sys.exit(2)
